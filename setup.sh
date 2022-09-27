b548fca24d63a32876ee0ef323aea889855a9a76
richarah <richarah@stud.ntnu.no>
2022-09-28 00:58:54 +0200

    Added gitignore

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+# Setup Alproot env
 cd alproot && ./alproot-setup.sh
+./alproot.sh apk add alpine-sdk
 
-cp -vr ../scripts alproot/env/scripts
+cp -vr ../scripts env
 
 ./alproot.sh ./scripts/install-icecream.sh
